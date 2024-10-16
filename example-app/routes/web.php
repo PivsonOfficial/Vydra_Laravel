21dd95b4e20a92ba2462043e75b1fe745c51db73
Humsus <ondra.vydra10@seznam.cz>
2024-10-16 08:28:21 +0200

    OpravenoFunkcniNazdar

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'products/index']);
+Route::get('/products', [ProductController::class, 'index'])->name ('products.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
