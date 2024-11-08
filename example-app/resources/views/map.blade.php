a82357ed39cc39886081ea8b2f0b9821e2087e33
Humsus <ondra.vydra10@seznam.cz>
2024-11-08 16:14:00 +0100

    mapaUprava2

--- example-app/resources/views/map.blade.php
+++ example-app/resources/views/map.blade.php
@@ -6,13 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mapa Chánova</title>
-    <!-- API klíč vložte na místo YOUR_GOOGLE_MAPS_API_KEY -->
     <script src="https://maps.googleapis.com/maps/api/js?key=ichtyl"></script>
     <style>
-        /* Nastavte rozměry pro mapu */
         #map {
-            height: 500px; /* Výška mapy */
-            width: 100%;   /* Šířka mapy */
+            height: 500px; 
+            width: 100%;   
         }
     </style>
 </head>
@@ -22,16 +20,13 @@
 
     <script>
         function initMap() {
-            // Souřadnice pro Chánov, Most, Česko
             const chanovPosition = { lat: 50.507251, lng: 13.653997 };
 
-            // Inicializace mapy
             const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15, // Nastavení úrovně přiblížení
+                zoom: 15, 
                 center: chanovPosition,
             });
 
-            // Přidání markeru na pozici Chánova
             new google.maps.Marker({
                 position: chanovPosition,
                 map: map,
@@ -39,7 +34,6 @@
             });
         }
 
-        // Spuštění inicializace mapy po načtení stránky
         window.onload = initMap;
     </script>
 
